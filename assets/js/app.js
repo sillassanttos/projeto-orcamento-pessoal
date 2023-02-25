37ca5edea14dd523c00c4b9e57f085c1a3a4e17e
Silas Santos <sillassanttos@outlook.com>
2023-02-25 14:57:14 -0400

    Adicionado mensagens de erro e confirmação dinamicamente

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,52 @@ class Despesa
     this.tipo = tipo
     this.descricao = descricao
     this.valor = valor
+  }
 
+  validarDados()
+  {
+    for (let i in this)
+    {
+      if (this[i] == undefined || this[i] == '' || this[i] == null)
+      {
+        return false
+      }
+    }
+    return true
   }
 }
 
-function cadastrarDespesa() {
+class Bd
+{
+  constructor()
+  {
+    let id = localStorage.getItem('id')
 
+    if (id === null)
+    {
+      localStorage.setItem('id', 0)
+    }
+  }
+  getProximoId() {
+    let proximoId = localStorage.getItem('id')
+    return parseInt(proximoId) + 1
+  }
+  gravar(d)
+  {
+    let id = this.getProximoId()
+
+    localStorage.setItem(id, JSON.stringify(d))
+
+    localStorage.setItem('id', id)
+  }
+}
+
+let bd = new Bd()
+
+function cadastrarDespesa()
+{
+
+  // Recupera as informações da tela
   let ano = document.getElementById('ano')
   let mes = document.getElementById('mes')
   let dia = document.getElementById('dia')
@@ -21,6 +61,7 @@ function cadastrarDespesa() {
   let descricao = document.getElementById('descricao')
   let valor = document.getElementById('valor')
 
+  // Adiciona as informacoes da tela no objeto despesa
   let despesas = new Despesa(
     ano.value,
     mes.value,
@@ -30,6 +71,28 @@ function cadastrarDespesa() {
     valor.value
   )
 
-  console.log(despesas)
+  if (despesas.validarDados())
+  {
+    bd.gravar(despesas)
+
+    document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+    document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+    document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+    document.getElementById('modal_btn').innerHTML = 'Voltar'
+    document.getElementById('modal_btn').className = 'btn btn-success'
+
+    $('#dialog').modal('show')
+  }
+  else
+  {
+    document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+    document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+    document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente.'
+    document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+    document.getElementById('modal_btn').className = 'btn btn-danger'
+
+    $('#dialog').modal('show')
+  }
 
 }
+
